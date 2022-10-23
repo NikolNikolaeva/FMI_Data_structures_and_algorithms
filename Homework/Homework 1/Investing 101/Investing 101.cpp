923fcf75604a7d90c7844f48aee9d110d8395878
Nikol Nikolaeva <nikol_lea@abv.bg>
2022-10-23 14:45:11 +0300

    Added homework

---
+++ Homework/Homework 1/Investing 101/Investing 101.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	std::vector<int> arr(N);
+	for (size_t i = 0; i < N; i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	int income = 0;
+	int minElementIndex = 0;
+	bool buy = true;
+
+	for (size_t i = 0; i < N; i++)
+	{
+
+		if (buy == false)
+		{
+			if (i == N - 1)
+			{
+				if (arr[i] - arr[minElementIndex] > 0)
+					income += arr[i] - arr[minElementIndex];
+				break;
+			}
+			if ((arr[i + 1] < arr[i]) && arr[minElementIndex] < arr[i])
+			{
+				income += arr[i] - arr[minElementIndex];
+				buy = true;
+			}
+			continue;
+		}
+
+		if (buy == true)
+		{
+			if (i == N - 1)
+				break;
+			if (arr[i + 1] > arr[i])
+			{
+				minElementIndex = i;
+				buy = false;
+			}
+			continue;
+		}
+
+	}
+
+	std::cout << income;
+}
+
