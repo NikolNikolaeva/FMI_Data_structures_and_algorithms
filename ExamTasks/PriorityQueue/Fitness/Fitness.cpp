415a70886cff26217aedb486b75f7427409e5e0f
Nikol Nikolaeva <nikol_lea@abv.bg>
2022-12-06 22:44:54 +0200

    Added exam tasks

---
+++ ExamTasks/PriorityQueue/Fitness/Fitness.cpp
@@ -0,0 +1,61 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+struct TimeWorkOut {
+    int timeIn;
+    int timeOut;
+};
+
+struct ComparatorInt {
+    bool operator()(const TimeWorkOut& t1, const TimeWorkOut& t2)
+    {
+        return t1.timeIn > t2.timeIn;
+    }
+};
+
+std::priority_queue<TimeWorkOut, std::vector<TimeWorkOut>, ComparatorInt> pqIntervals;
+std::priority_queue<int, std::vector<int>, std::greater<int>> pqEnds;
+
+int main() {
+
+    int n;
+    std::cin >> n;
+
+    int maxEnd = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int start, end;
+        std::cin >> start >> end;
+        pqIntervals.push({ start,end });
+        if (end > maxEnd)
+            maxEnd = end;
+    }
+
+    int maxPeople = 0;
+
+    for (int i = 0; i < maxEnd; i++)
+    {
+        while (!pqEnds.empty() && pqEnds.top() <= i)
+        {
+            pqEnds.pop();
+        }
+
+        while (!pqIntervals.empty() && pqIntervals.top().timeIn <= i && pqIntervals.top().timeOut >= i)
+        {
+            pqEnds.push(pqIntervals.top().timeOut);
+            pqIntervals.pop();
+        }
+        if (maxPeople < pqEnds.size())
+        {
+            maxPeople = pqEnds.size();
+        }
+    }
+
+    std::cout << maxPeople<<'\n';
+    return 0;
+}
---
+++ Exercises/PriorityQueue/Requests/Requests.cpp
@@ -0,0 +1,76 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+struct Request
+{
+    int value;
+    int timeIn;
+};
+
+struct Compare {
+    bool operator()(const Request& r1, const Request& r2)
+    {
+        return r1.value > r2.value;
+    }
+};
+
+std::priority_queue<Request, std::vector<Request>, Compare> pq;
+
+int main() {
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int N;
+    std::cin >> N;
+
+    int size = N;
+    int time = 0;
+    std::vector<int> timeRequest;
+
+    int value;
+    std::cin >> value;
+    pq.push({ value, 0 });
+    time += value;
+    int top = pq.top().value;
+    timeRequest.push_back(time - pq.top().timeIn);
+    pq.pop();
+    size--;
+    int index = 1;
+
+    while (size != 0)
+    {
+
+        for (int i = 0; i < top; i++)
+        {
+            size--;
+            int val;
+            std::cin >> val;
+            pq.push({ val,index++ });
+            if (size == 0)
+                break;
+        }
+
+        top = pq.top().value;
+        time += top;
+        timeRequest.push_back(time - pq.top().timeIn);
+        pq.pop();
+    }
+
+    while (!pq.empty())
+    {
+        time += pq.top().value;
+        timeRequest.push_back(time - pq.top().timeIn);
+        pq.pop();
+    }
+
+    std::sort(timeRequest.begin(), timeRequest.end());
+    std::cout << timeRequest[0] << " " << timeRequest[size - 1];
+
+    return 0;
+}
