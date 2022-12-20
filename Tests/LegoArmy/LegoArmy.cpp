6ebaa4c0f2ba4f1127ca82d4df0c19d4374873c5
Nikol Nikolaeva <nikol_lea@abv.bg>
2022-12-20 10:45:04 +0200

    Added tasks from test

---
+++ Tests/LegoArmy/LegoArmy.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+#include <iostream>
+#include <queue>
+
+std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+
+int main() {
+
+	int n, min;
+	std::cin >> n >> min;
+
+	for (int i = 0; i < n; i++)
+	{
+		int value;
+		std::cin >> value;
+		pq.push(value);
+	}
+
+	int rounds = 0;
+	while (pq.top() < min && pq.size() > 1)
+	{
+		if (pq.top() < min)
+		{
+			int newValue = pq.top();
+			pq.pop();
+			newValue += 2 * pq.top();
+			pq.pop();
+			pq.push(newValue);
+		}
+
+		rounds++;
+	}
+
+	if (pq.size() == 1 && pq.top() < min)
+	{
+		std::cout << -1;
+		return 0;
+	}
+	std::cout << rounds;
+
+	return 0;
+}
